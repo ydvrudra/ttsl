33459deef319efbe74eec4c3e1440d8a470a90e7
ydvrudra <rudrapratapyadav141@gmail.com>
2025-12-10 12:54:32 +0530

    serverss

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const truckRoutes = require('./routes/truckRoutes.js');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = 5000;
 app.use(cors());
 app.use(express.json());
 
