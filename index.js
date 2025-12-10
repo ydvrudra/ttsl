8f3d6d2640d62aa026f794b2127e0a16ee056713
ydvrudra <rudrapratapyadav141@gmail.com>
2025-12-10 12:34:37 +0530

    server

--- index.js
+++ index.js
@@ -8,21 +8,10 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-app.post('/truck-suggest-api', (req, res) => {
-  res.json({ 
-    message: 'POST request received',
-    timestamp: new Date().toISOString(),
-    data: req.body
-  });
-});
-
-
 app.use("/api", truckRoutes);
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
-   console.log(`📡 API: http://localhost:${port}/api/truck/suggest`);
 });
 
 
